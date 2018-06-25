4fca82df05b10e354718c7d69ed4b16761143733
cky168 <sky__cky@hotmail.com>
2018-06-25 12:14:12 +0800

    c2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable,of } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  login$: Observable<boolean>;
+    constructor() { }
+    ngOnInit() {
+        this.login$ = of(true);
+    }
+    logout() {
+        this.login$ = of(false);
+    }
 
 }
